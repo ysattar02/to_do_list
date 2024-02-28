c52165a72186f1212f4fd202a90e8ab6f73b2160
ysattar02 <yusufsattar@gmail.com>
2024-02-28 10:29:35 -0500

    Project Setup

---
+++ cmake-build-debug/CMakeFiles/to_do_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/to_do_list.dir/main.cpp.o"
+  "to_do_list"
+  "to_do_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/to_do_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
